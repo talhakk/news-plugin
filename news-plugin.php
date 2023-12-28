96d05f5b26c31e06b8f0d00e2e8a0cf33024a0e6
Talha Khan <talhakhanandco@gmail.com>
2023-12-28 16:43:33 +0500

    Done API error Handling

--- news-plugin.php
+++ news-plugin.php
@@ -40,6 +40,45 @@ function newsplugin_activate() {
 }
 register_activation_hook( __FILE__, 'newsplugin_activate' );
 
+
+/**
+ * Register Taxonomy
+ */
+function create_news_taxonomy() {
+    register_taxonomy('country','news',array(
+        'hierarchical' => false,
+        'labels' => array(
+            'name' => _x( 'Countries', 'taxonomy general name' ),
+            'singular_name' => _x( 'Country', 'taxonomy singular name' ),
+            'menu_name' => __( 'Country' ),
+            'all_items' => __( 'Countries' ),
+            'edit_item' => __( 'Edit Country' ), 
+            'update_item' => __( 'Update Country' ),
+            'add_new_item' => __( 'Add Country' ),
+            'new_item_name' => __( 'New Country' ),
+        ),
+    'show_ui' => true,
+    'show_in_rest' => true,
+    'show_admin_column' => true,
+    ));
+    register_taxonomy('Language','news',array(
+        'hierarchical' => false,
+        'labels' => array(
+            'name' => _x( 'Languages', 'taxonomy general name' ),
+            'singular_name' => _x( 'Language', 'taxonomy singular name' ),
+            'menu_name' => __( 'Languages' ),
+            'all_items' => __( 'Languages' ),
+            'edit_item' => __( 'Edit Language' ), 
+            'update_item' => __( 'Update Language' ),
+            'add_new_item' => __( 'Add Language' ),
+            'new_item_name' => __( 'New Language' ),
+        ),
+    'show_ui' => true,
+    'show_in_rest' => true,
+    'show_admin_column' => true,
+    ));
+}
+add_action( 'init', 'create_news_taxonomy', 0 );
 /*
 Make dashboard page for settings
 */
@@ -49,7 +88,7 @@ function newsplugin_settings_page() {
         'Google News Api Settings',
         'News Plugin',
         'manage_options',
-        plugin_dir_path(__FILE__) . 'settings2.php',
+        plugin_dir_path(__FILE__) . 'settings.php',
         null,
         '',
         20
@@ -80,10 +119,13 @@ add_action( 'wp_ajax_search_news', 'search_news' );
 
 function search_news() {
 	$searchTerm =  $_POST['searchterm'];
+    $searchMax =  $_POST['maxnews'];
+    $searchLang =  $_POST['searchlang'];
+    $searchRegion =  $_POST['searchregion'];
     //retriving api key
     $apikey = get_option('newsplugin_save_api_key');
-    $url = "https://gnews.io/api/v4/search?q=$searchTerm&lang=en&country=us&max=10&apikey=$apikey";
-
+    //adding double quotes to output search terms with spaces
+    $url = "https://gnews.io/api/v4/search?q="."\"$searchTerm\""."&lang=$searchLang&country=$searchRegion&max=$searchMax&apikey=$apikey";
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -91,37 +133,63 @@ $data = json_decode(curl_exec($ch), true);
 curl_close($ch);
 $articles = $data['articles'];
 
-for ($i = 0; $i < count($articles); $i++) {
-   /* echo "Title: " . $articles[$i]['title'] . "\n";
-    echo "Description: " . $articles[$i]['description'] . "\n";
-    echo "Description: " . $articles[$i]['content'] . "\n";
-*/
-    // Create post object
-$new_post = array(
-    'post_type' => 'news',
-    'post_title'    => wp_strip_all_tags( $articles[$i]['title'] ),
-    'post_content'  => $articles[$i]['content'],
-    'post_excerpt'  => $articles[$i]['description'],
-    'post_status'   => 'publish',
-    'post_author'   => 1
-    );
+$errors = $data['errors'];
+if(!$errors==null){
+    for ($i = 0; $i < count($errors); $i++) {
+        //echo "Error: " . $errors[$i][0] . "\n";
+       echo $errors[$i];
+     
+    }
+}else{
+    for ($i = 0; $i < count($articles); $i++) {
+        // Create post object
+       // $create_taxonomy=wp_insert_term($searchRegion, 'country');
+    /*
+    if ( ! is_wp_error( $create_taxonomy ) )
+    {
+        // Get term_id, set default as 0 if not set
+        $cat_id = isset( $create_taxonomy['term_id'] ) ? $create_taxonomy['term_id'] : 0;
+        // ... etc ...
+    }
+    else
+    {
+         // Trouble in Paradise:
+         echo $create_taxonomy->get_error_message();
+    }*/
     
-    // Insert post into the database
-    // Use $wp_error set to true for error handling
-$post_check = wp_insert_post($new_post, true); 
+    $new_post = array(
+        'post_type' => 'news',
+        'post_title'    => wp_strip_all_tags( $articles[$i]['title'] ),
+        'post_content'  => $articles[$i]['content'],
+        'post_excerpt'  => $articles[$i]['description'],
+        'post_status'   => 'publish',
+        'post_author'   => 1
+        );
+        
+        // Insert post into the database
+        // Use $wp_error set to true for error handling
+    $post_check = wp_insert_post($new_post, true); 
+    // add taxonomy data, but it will add country again each time searched 
+    
+    /* problem here is maybe no taxonomy at this time
+    $taxonomy = 'country';
+    $termObj  = get_term_by( 'id', 7, $taxonomy);
+    wp_set_object_terms($post_check, $termObj, $taxonomy);
+    */
+    
+    // Check if there was an error during post insertion
+    if (is_wp_error($post_check)) {
+        // Error occurred while inserting the post
+        echo "Error: " . $post_check->get_error_message();
+    } else {
+        // The post was successfully inserted, and $post_id contains the post ID
+        echo "Post inserted successfully. New Post ID: " . $post_check;
+    }
+    }//for loop
+}//if(!$errors==null){
 
-// Check if there was an error during post insertion
-if (is_wp_error($post_check)) {
-    // Error occurred while inserting the post
-    echo "Error: " . $post_check->get_error_message();
-} else {
-    // The post was successfully inserted, and $post_id contains the post ID
-    echo "Post inserted successfully. New Post ID: " . $post_check;
-}
-}//for loop
-    
+
     //returing the response
-    echo $searchTerm;
 	wp_die(); // this is required to terminate immediately and return a proper response
 }
 /**
--- settings.php
+++ settings.php
@@ -1,4 +1,5 @@
 <?php
+//e335cb473ab90265a531a14120ef424c
 $apikey = get_option('newsplugin_save_api_key');
 // adding js in footer for simplicity and to avoid loading it on the whole site
 add_action( 'admin_footer', 'newsplugin_api_javascript' ); 
@@ -12,14 +13,10 @@ function newsplugin_api_javascript() { ?>
     $('#form-button-submit').on('click', function(e){
        e.preventDefault();
        var key =$("#apikey").val();
-    
-
-
 		var data = {
 			'action': 'save_api_key',
 			'newsapikey': key
-		};
-    
+		};  
 		// since 2.8 ajaxurl is always defined in the admin header and points to admin-ajax.php
 		jQuery.post(ajaxurl, data, function(response) {
 			document.getElementById('newsplugin_submit_message').innerHTML='Your API Key: '+response+ '\n has been saved successfully';         
@@ -32,12 +29,17 @@ function newsplugin_api_javascript() { ?>
 	$('#form-search-button-submit').on('click', function(e){
        e.preventDefault();
        var searchinput =$("#news-search-input").val();
-    
-
-
+       var searchmax=$("#news-max-input").val();
+	   var searchlang=$("#news-language-input").val();
+	   var searchcountry=$("#news-countries-inpu").val();
+	 
 		var data = {
 			'action': 'search_news',
-			'searchterm': searchinput
+			'searchterm': searchinput,
+			'maxnews': searchmax,
+			'searchlang':searchlang,
+			'searchregion':searchcountry
+			
 		};
     
 		// since 2.8 ajaxurl is always defined in the admin header and points to admin-ajax.php
@@ -53,12 +55,27 @@ function newsplugin_api_javascript() { ?>
 ?> 
 <form action="#" method="post">
 <input id="apikey" type="text" name="apikey" value="<?php echo $apikey ?>">
-<input id="form-button-submit" type="submit">
+<input id="form-button-submit" type="submit" value="Save API">
 </form>
 <p id="newsplugin_submit_message"></p>
 
 <form action="#" method="post">
 <input id="news-search-input" type="text">
-<input id="form-search-button-submit" type="submit">
+<input id="news-max-input" type="number" value="10">
+<label for="language">Select a language:</label>
+  <select name="language" id="news-language-input">
+    <option value="ar">Arabic</option>
+    <option value="zh">Chinese</option>
+    <option value="en" selected>English</option>
+    <option value="hi">Hindi</option>
+  </select>
+  <label for="countries">Select a Country:</label>
+  <select name="countries" id="news-countries-input">
+    <option value="cn">China</option>
+    <option value="pk">Pakistan</option>
+    <option value="us" selected>United States</option>
+    <option value="in">India</option>
+  </select>
+<input id="form-search-button-submit" type="submit" value="Generate News">
 </form>
 <p id="newsplugin_search_submit_message"></p>
